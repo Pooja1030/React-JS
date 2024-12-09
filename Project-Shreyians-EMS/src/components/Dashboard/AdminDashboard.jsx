e5a578688147d5122a42c1aff32bafe640808f3a
Pooja <poojasawant1030@gmail.com>
2024-12-09 22:17:00 +0530

    implemented the dashboards

--- Project-Shreyians-EMS/src/components/Dashboard/AdminDashboard.jsx
+++ Project-Shreyians-EMS/src/components/Dashboard/AdminDashboard.jsx
@@ -1,12 +1,17 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import Header from '../other/Header'
+import CreateTask from '../other/CreateTask'
+import AllTask from '../other/AllTask'
 
-const AdminDashboard = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const AdminDashboard = (props) => {
+    return (
+        <div className='h-screen w-full p-7'>
+            <Header changeUser={props.changeUser} />
+            <CreateTask />
+            <AllTask />
+        </div>
+    )
 }
 
 export default AdminDashboard
